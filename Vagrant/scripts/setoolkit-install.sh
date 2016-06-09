73d69e0b493a8d744e033f698bdf0e7eec849cd8
Cahya Wirawan <cahya.wirawan@gmail.com>
2016-06-09 12:18:51 +0200

    Fixed setoolkit directory check and changed metasploit directory in setoolkit's configuration

--- Vagrant/scripts/setoolkit-install.sh
+++ Vagrant/scripts/setoolkit-install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-if [ ! -d /opt/social-engineer-toolkit ]; then
+if [ ! -d /usr/share/setoolkit ]; then
   cd /opt
   git clone https://github.com/trustedsec/social-engineer-toolkit.git social-engineer-toolkit
   cd social-engineer-toolkit
   python setup.py install
+  sed -i 's/METASPLOIT_PATH=.*/METASPLOIT_PATH=\/opt\/metasploit/' /etc/setoolkit/set.config
 fi
